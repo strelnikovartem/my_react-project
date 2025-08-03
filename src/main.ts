94dee5834757cfce66a13a12ff9d2f087f200d96
Artem Strelnikov <piramida1237@gmail.com>
2025-08-03 11:42:16 +0200

    add_jacob

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,3 @@
-import "./style.css";
-interface User {
-  id: number;
-  name: string;
-}
-
-const getUserNames = (users: User[]): string[] => {
-  return users.map((user) => user.name);
-};
-
-const userList: User[] = [
-  { id: 1, name: "Alice" },
-  { id: 2, name: "Bob" },
-  { id: 3, name: "Charlie" },
-];
-
-const names = getUserNames(userList);
-console.log(names); // ['Alice', 'Bob', 'Charlie']
-
-interface Order {
-  username: string;
-  email: string;
-  total: number;
-}
-
-const order1: Order = {
-  username: "string",
-  email: "dd@mail.com",
-  total: 2,
+const jacob = {
+  username: "Jacob",
 };
